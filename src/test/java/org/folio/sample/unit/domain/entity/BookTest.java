deacb3b5f07d29b7fbe15e84b6bffdf8be7486f9
danetsao <danetsaoo@gmail.com>
2023-06-05 11:21:28 -0500

    Added method source

--- src/test/java/org/folio/sample/unit/domain/entity/BookTest.java
+++ src/test/java/org/folio/sample/unit/domain/entity/BookTest.java
@@ -6,67 +6,48 @@ import static org.hamcrest.Matchers.is;
 import java.time.LocalDate;
 import org.folio.sample.domain.entity.Book;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BookTest {
 
-  @Test
-  void testLeapYear() {
+  @MethodSource
+  void testIsPublishedInLeapYear(int year, boolean expected) {
     Book testBook = Book
       .builder()
       .name("Sample book")
-      .publishedDate(LocalDate.of(2020, 1, 1))
+      .publishedDate(LocalDate.of(year, 1, 1))
       .build();
-    assertThat(testBook.isPublishedInLeapYear(), is(true));
+    assertThat(testBook.isPublishedInLeapYear(), is(expected));
+  }
+
+  @Test
+  void testLeapYear() {
+    testIsPublishedInLeapYear(2020, true);
   }
 
   @Test
   void testNotLeapYear() {
-    Book testBook = Book
-      .builder()
-      .name("Sample book")
-      .publishedDate(LocalDate.of(2001, 1, 1))
-      .build();
-    assertThat(testBook.isPublishedInLeapYear(), is(false));
+    testIsPublishedInLeapYear(2001, false);
   }
 
   @Test
   void testNotLeapYear2() {
-    Book testBook = Book
-      .builder()
-      .name("Sample book")
-      .publishedDate(LocalDate.of(99999, 1, 1))
-      .build();
-    assertThat(testBook.isPublishedInLeapYear(), is(false));
+    testIsPublishedInLeapYear(99999, false);
   }
 
   @Test
   void testLeapYear2() {
-    Book testBook = Book
-      .builder()
-      .name("Sample book")
-      .publishedDate(LocalDate.of(2000, 1, 1))
-      .build();
-    assertThat(testBook.isPublishedInLeapYear(), is(true));
+    testIsPublishedInLeapYear(2000, true);
   }
 
   @Test
   void testLeapYear3() {
-    Book testBook = Book
-      .builder()
-      .name("Sample book")
-      .publishedDate(LocalDate.of(1900, 1, 1))
-      .build();
-    assertThat(testBook.isPublishedInLeapYear(), is(false));
+    testIsPublishedInLeapYear(1900, false);
   }
 
   @Test
   void testLeapYear4() {
-    Book testBook = Book
-      .builder()
-      .name("Sample book")
-      .publishedDate(LocalDate.of(1, 1, 1))
-      .build();
-    assertThat(testBook.isPublishedInLeapYear(), is(false));
+    testIsPublishedInLeapYear(1, false);
   }
   
 }
