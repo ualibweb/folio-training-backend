bf2c3a302b6e961b9f28164b057dd621fd59114c
danetsao <danetsaoo@gmail.com>
2023-06-05 09:12:22 -0500

    Fixed put method and spacing

--- src/main/java/org/folio/sample/controller/BookController.java
+++ src/main/java/org/folio/sample/controller/BookController.java
@@ -1,6 +1,5 @@
 package org.folio.sample.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
@@ -38,16 +37,18 @@ public class BookController implements BooksApi {
 
     if (Boolean.TRUE.equals(onlyLeapYears)) {
       return new ResponseEntity<>(
-          bookService
-              .getAllBooksPublishedInLeapYears()
-              .stream()
-              .map(bookMapper::toDto)
-              .toList(),
-          HttpStatus.OK);
+        bookService
+          .getAllBooksPublishedInLeapYears()
+          .stream()
+          .map(bookMapper::toDto)
+          .toList(),
+        HttpStatus.OK
+      );
     } else {
       return new ResponseEntity<>(
-          bookService.getAllBooks().stream().map(bookMapper::toDto).toList(),
-          HttpStatus.OK);
+        bookService.getAllBooks().stream().map(bookMapper::toDto).toList(),
+        HttpStatus.OK
+      );
     }
   }
 
@@ -57,8 +58,9 @@ public class BookController implements BooksApi {
     log.info("Called POST /books with book={}", book);
 
     return new ResponseEntity<>(
-        bookMapper.toDto(bookService.createBook(bookMapper.fromDto(book))),
-        HttpStatus.CREATED);
+      bookMapper.toDto(bookService.createBook(bookMapper.fromDto(book))),
+      HttpStatus.CREATED
+    );
   }
 
   /** {@inheritDoc} */
@@ -67,11 +69,13 @@ public class BookController implements BooksApi {
     log.info("Called GET /books/{}", bookId);
 
     return new ResponseEntity<>(
-        bookMapper.toDto(
-            bookService
-                .getBookById(bookId)
-                .orElseThrow(() -> new NotFoundException("Book not found"))),
-        HttpStatus.OK);
+      bookMapper.toDto(
+        bookService
+          .getBookById(bookId)
+          .orElseThrow(() -> new NotFoundException("Book not found"))
+      ),
+      HttpStatus.OK
+    );
   }
 
   /** {@inheritDoc} */
@@ -79,23 +83,13 @@ public class BookController implements BooksApi {
   public ResponseEntity<BookDTO> updateBook(UUID bookId, BookForCreationDTO book) {
     log.info("Called PUT /books/{}", bookId);
 
-    // Get params for new book
-    String newBookName = book.getName();
-    LocalDate newBookPublishedDate = book.getPublishedDate();
-
-    // Get old book, throw exception if not found and respond 404
-    BookDTO oldBook = bookMapper.toDto(
-        bookService
-            .getBookById(bookId)
-            .orElseThrow(() -> new NotFoundException("Book not found")));
-            
-    // Get old book's id
-    UUID oldBookId = oldBook.getId();
-
-    // Update book, call updateBook method from BookService
     return new ResponseEntity<>(
-        bookMapper.toDto(
-            bookService.updateBook(oldBookId, newBookName, newBookPublishedDate)),
-        HttpStatus.OK);
+      bookMapper.toDto(
+        bookService
+          .updateBook(bookId, bookMapper.fromDto(book))
+          .orElseThrow(() -> new NotFoundException("Book not found"))
+      ),
+      HttpStatus.OK
+    );
+    }
   }
-}
--- src/main/java/org/folio/sample/service/BookService.java
+++ src/main/java/org/folio/sample/service/BookService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+
+import org.folio.sample.domain.dto.BookForCreationDTO;
 import org.folio.sample.domain.entity.Book;
 import org.folio.sample.repository.BookRepository;
+import org.folio.spring.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,10 +35,18 @@ public class BookService {
   public Book createBook(Book book) {
     return bookRepository.save(book);
   }
-  public Book updateBook(UUID id, String name, LocalDate publishedDate) {
-    Book curBook = bookRepository.getReferenceById(id);
-    curBook.setName(name);
-    curBook.setPublishedDate(publishedDate);
-    return bookRepository.save(curBook);
+
+  public Optional<Book> updateBook(UUID bookId, Book book) {
+    // Get the book from the database, throw NotFoundException if not found
+    Book bookFromDb = bookRepository.findById(bookId).orElseThrow(() -> new NotFoundException("Book not found"));
+
+    // Update the book
+    bookFromDb.setName(book.getName());
+    bookFromDb.setPublishedDate(book.getPublishedDate());
+
+    
+    // Save the updated book
+    return Optional.of(bookRepository.save(bookFromDb));
   }
+
 }
