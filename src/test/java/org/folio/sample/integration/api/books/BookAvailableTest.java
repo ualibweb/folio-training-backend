2e4c8d4f61ffefa6782784098308b1500c27e323
danetsao <danetsaoo@gmail.com>
2023-06-06 12:06:42 -0500

    Removed unnecesary code from tests

--- src/test/java/org/folio/sample/integration/api/books/BookAvailableTest.java
+++ src/test/java/org/folio/sample/integration/api/books/BookAvailableTest.java
@@ -4,13 +4,11 @@ import io.restassured.common.mapper.TypeRef;
 import io.restassured.response.Response;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 import org.folio.sample.domain.dto.BookDTO;
 import org.folio.sample.domain.dto.BookForCreationDTO;
 import org.folio.sample.integration.AbstractBaseApiTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpStatus;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,21 +19,6 @@ import static org.hamcrest.Matchers.hasSize;
 
 
 class BookAvailableTest extends AbstractBaseApiTest {
-
-  @MethodSource
-  // localdate is optional and deffault value is 2000-01-01
-  Response createBook(String name) {
-    Response postResponse = ra()
-      .body(
-        BookForCreationDTO
-          .builder()
-          .name(name)
-          .publishedDate(LocalDate.of(2000, 1, 1))
-          .build()
-      )
-      .post(getRequestUrl("books"));
-    return postResponse;
-  }
 
   @Test
   void testEmptyGet() {
@@ -49,23 +32,6 @@ class BookAvailableTest extends AbstractBaseApiTest {
       .getBody()
       .as(new TypeRef<List<BookDTO>>() {});
     assertThat(collection, hasSize(0));
-  }
-
-  @Test
-  void testNotFound() {
-    ra()
-      .pathParam("id", UUID.fromString("00000000-0000-0000-0000-000000000000"))
-      .get(getRequestUrl("books/{id}"))
-      .then()
-      .statusCode(is(HttpStatus.NOT_FOUND.value()));
-  }
-
-  @Test
-  void isAvailableBook() {
-    Response r1 = createBook("Book 1");
-    r1.then().statusCode(is(HttpStatus.CREATED.value()));
-    // Check if book is available
-    assertThat(r1.as(BookDTO.class).isIsAvailable(), is(equalTo(true)));
   }
 
   @Test
