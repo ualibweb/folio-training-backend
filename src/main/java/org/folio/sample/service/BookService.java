3dde174fe14fea2cfea2814313c5833284520be6
Chris Hellen <chellen@ua.edu>
2024-04-08 08:55:36 -0500

    Adapted PUT logic to exist within bookService

--- src/main/java/org/folio/sample/service/BookService.java
+++ src/main/java/org/folio/sample/service/BookService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+
 import org.folio.sample.domain.entity.Book;
 import org.folio.sample.repository.BookRepository;
 import org.folio.spring.exception.NotFoundException;
