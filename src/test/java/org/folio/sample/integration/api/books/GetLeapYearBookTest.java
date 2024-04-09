f1d6f7cf1cd2df6e12450678971f000ac563cccf
Chris Hellen <chellen@ua.edu>
2024-04-09 12:39:01 -0500

    Completed training module 02-testing

--- src/test/java/org/folio/sample/integration/api/books/GetLeapYearBookTest.java
+++ src/test/java/org/folio/sample/integration/api/books/GetLeapYearBookTest.java
@@ -15,7 +15,7 @@ import org.folio.sample.integration.AbstractBaseApiTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-class GetLeapYearBookTest extends AbstractBaseApiTest {
+public class GetLeapYearBookTest extends AbstractBaseApiTest {
   @Test
   void testEmptyGet(){
     Response response = ra()
